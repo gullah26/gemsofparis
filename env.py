ceaaca2e1b282f057f8cdf0fe5e7dc0ccebe0f2d
Seun Ajetomobi <fullstackfory@gmail.com>
2022-11-23 22:28:53 +0000

    Add environ variables to env.py

---
+++ env.py
@@ -0,0 +1,16 @@
+"""app variables environment"""
+
+
+import os
+os.environ["AWS_ACCESS_KEY_ID"] = ""
+os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["DATABASE_URL"] = ""
+os.environ["EMAIL_HOST_PASS"] = ""
+os.environ["EMAIL_HOST_USER"] = ""
+os.environ["MAILCHIMP_KEY"] = ""
+os.environ["MAILCHIMP_LIST_ID"] = ""
+os.environ["SECRET_KEY"] = ""
+os.environ["STRIPE_PUBLIC_KEY"] = ""
+os.environ["STRIPE_SECRET_KEY"] = ""
+os.environ["STRIPE_WH_SECRET"] = ""
+os.environ["USE_AWS"] = "TRUE"
