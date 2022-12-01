3344816b3065ed55815f67185de8066022c0dcff
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-01 01:12:41 +0000

    customize aarea to preview product images

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 # Create your models here.
 
@@ -32,3 +33,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def img_preview(self): #new
+        return mark_safe(f'<img src="{self.image.url}" width = "300"/>')
