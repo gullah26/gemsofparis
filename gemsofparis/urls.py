42676f7a3c9ce18d9d67d8d546c925122b1a732c
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-03 23:49:17 +0000

    Add bad context and delivery threshold logic

--- gemsofparis/urls.py
+++ gemsofparis/urls.py
@@ -1,7 +1,6 @@
-"""gemsofparis URL Configuration
-
+"""boutique_ado URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -22,5 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-     path('products/', include('products.urls')),
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
