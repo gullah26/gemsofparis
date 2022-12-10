f8e119320a2844b6c5056e469bb75b004565e2de
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-10 05:18:47 +0000

    Add checkout view , forms and template

--- gemsofparis/settings.py
+++ gemsofparis/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -73,8 +76,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  #  required by Allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
