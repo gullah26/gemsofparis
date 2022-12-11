3a056f9d0e313aa02a41d2252095c877049c9133
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-11 22:40:55 +0000

    Add checkout success logic

--- env.py
+++ env.py
@@ -1,16 +1,16 @@
-"""app variables environment"""
-
+#  app variables environment
 
 import os
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
-os.environ["DATABASE_URL"] = ""
-os.environ["EMAIL_HOST_PASS"] = ""
-os.environ["EMAIL_HOST_USER"] = ""
-os.environ["MAILCHIMP_KEY"] = ""
-os.environ["MAILCHIMP_LIST_ID"] = ""
-os.environ["SECRET_KEY"] = ""
-os.environ["STRIPE_PUBLIC_KEY"] = ""
-os.environ["STRIPE_SECRET_KEY"] = ""
-os.environ["STRIPE_WH_SECRET"] = ""
-os.environ["USE_AWS"] = "TRUE"
+
+# os.environ["AWS_ACCESS_KEY_ID"] = ""
+# os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+# os.environ["DATABASE_URL"] = ""
+# os.environ["EMAIL_HOST_PASS"] = ""
+# os.environ["EMAIL_HOST_USER"] = ""
+# os.environ["MAILCHIMP_KEY"] = ""
+# os.environ["MAILCHIMP_LIST_ID"] = ""
+# os.environ["SECRET_KEY"] = ""
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51LKMRbFdNsiAbeg4QWXBLIA1LVl9EHURwMdkqPrEcN2MJP9VDVMsSjLXBoLO7IPASeVbLveAZsbyHWwZnOmVjQok0019uBv4Uu"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51LKMRbFdNsiAbeg4XsFJG5ZS1iSGiTxA73uoUxzayCTj3y6CykZUhN36hcFt6BVxdbSROseY9OOGxoUpMZjN5DOY00e5Ok7Hhn"
+# os.environ["STRIPE_WH_SECRET"] = ""
+# os.environ["USE_AWS"] = "TRUE"
