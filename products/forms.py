0660965f8e6c468a7e5009abccfbca90284c200d
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-12 21:54:09 +0000

    product admin - product form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -8,10 +7,6 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
-    image = forms.ImageField(label='Image',
-                             required=False,
-                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
