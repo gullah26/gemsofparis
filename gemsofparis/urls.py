6ba240844a779813382f00b3958264bc6e7c0095
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-13 10:09:38 +0000

    Deployment part 3

--- gemsofparis/urls.py
+++ gemsofparis/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado URL Configuration
+"""gems of Paris URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
