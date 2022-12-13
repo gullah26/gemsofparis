eae386b5729765e1824ebe21d2a1581c931ea783
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-13 09:40:46 +0000

    Deployment part 2

--- env.py
+++ env.py
@@ -2,15 +2,15 @@
 
 import os
 
-# os.environ["AWS_ACCESS_KEY_ID"] = ""
-# os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIA5VW7MKEYJKU2NVP5"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
 os.environ["DATABASE_URL"] = "postgres://lobsocxg:oGSPCE6p_OPBJtyv48Y8k3RgoOxJTMfv@mouse.db.elephantsql.com/lobsocxg"
 # os.environ["EMAIL_HOST_PASS"] = ""
 # os.environ["EMAIL_HOST_USER"] = ""
 # os.environ["MAILCHIMP_KEY"] = ""
 # os.environ["MAILCHIMP_LIST_ID"] = ""
-os.environ["SECRET_KEY"] = ""
+os.environ["SECRET_KEY"] = "Project5"
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51LKMRbFdNsiAbeg4QWXBLIA1LVl9EHURwMdkqPrEcN2MJP9VDVMsSjLXBoLO7IPASeVbLveAZsbyHWwZnOmVjQok0019uBv4Uu"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51LKMRbFdNsiAbeg4XsFJG5ZS1iSGiTxA73uoUxzayCTj3y6CykZUhN36hcFt6BVxdbSROseY9OOGxoUpMZjN5DOY00e5Ok7Hhn"
 os.environ["STRIPE_WH_SECRET"] = "whsec_YJ5XXGttkuvNA0TOEEbwFQDLCflxnftE"
-# os.environ["USE_AWS"] = "TRUE"
+os.environ["USE_AWS"] = "True"
--- gemsofparis/settings.py
+++ gemsofparis/settings.py
@@ -11,22 +11,35 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
-from pathlib import Path
+    
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n2li2i+@6bd%vm!qx60kw-60p54zka-fhm7@)_u5(vsh7vvf(1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True  #'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gemsofparis.herokuapp.com', 'localhost']
+
+
+# CSRF_TRUSTED_ORIGINS = ['https://example.com']
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000'
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+]
 
 
 # Application definition
@@ -50,7 +63,9 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,6 +78,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gemsofparis.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -178,6 +195,30 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'gemsofparis'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 # free delivery threshold
 
 FREE_DELIVERY_THRESHOLD = 50
@@ -188,6 +229,21 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'gemsofparis@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
 DEFAULT_FROM_EMAIL = 'gemsofparis@example.com'
 
 
