127cf9f0a2657e5e1268bac68481f8bf89327f93
Seun Ajetomobi <fullstackfory@gmail.com>
2022-12-13 00:15:27 +0000

    deployment commit 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gemsofparis.wsgi:application
--- gemsofparis/settings.py
+++ gemsofparis/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n2li2i+@6bd%vm!qx60kw-60p54zka-fhm7@)_u5(vsh7vvf(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gemsofparis.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +122,18 @@ WSGI_APPLICATION = 'gemsofparis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
